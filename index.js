c91f4610a7670c3e7a10294f9058181567dcd6fb
Shakhruz Ashirov <shakhruz@gmail.com>
2018-06-16 10:16:22 +0500

    account database
    added mongodb, mongoose database connected with mlab to store account for monitoring, added model for account and two commands /add xxxx and /list to add and list all accounts

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const { Composer, log, session, Markup } = require('micro-bot')
+require('dotenv').config()
 const msg = require('./messages')
-const Eos = require('eosjs')
 const producerSearch = require('./lib/producerSearch')
 
-// Default configuration
+// Connect to MongoDB
+const mongoose = require('mongoose')
+const Account = require('./models/accountmodel')
+mongoose.connect(process.env.MONGOLAB_URI)
+
+const db = mongoose.connection
+mongoose.Promise = global.Promise
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', function () {
+  console.log('MongoDB connected')
+})
+
+// Connect to EOS
+const Eos = require('eosjs')
 const config = {
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
   httpEndpoint: 'https://eos.greymass.com',
@@ -14,6 +26,9 @@ const config = {
 }
 
 const eos = Eos(config)
+
+// Connect Telegram Bot
+const { Composer, log, session, Markup } = require('micro-bot')
 const app = new Composer()
 
 app.use(log())
@@ -45,6 +60,43 @@ app.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
 })
 
 app.command('help', ctx => ctx.replyWithMarkdown(msg.help))
-// app.hears(/(так)|(ишо)/i, ctx => ctx.replyWithMarkdown(msg.formatQuote(getQuote({ details: true }))))
+
+// Add account to the monitoring list
+app.command('add', ctx => {
+  const {text} = ctx.message
+  const index = text.trim().indexOf(' ')
+  const query = index > 0 ? text.substr(index + 1) : ''
+
+  if (query.length > 0) {
+    // check if the account exists
+    console.log('find account ' + query)
+    Account.findOne({account_name: query}, (err, acc) => {
+      if (!err && !acc) {
+        // account not found
+        console.log('account ', query, ' not found, adding it...')
+        var newAcc = new Account({ account_name: query })
+        newAcc.save(function (err, acc) {
+          if (err) return console.error(err)
+          console.log('added new account: ', acc)
+          ctx.replyWithMarkdown('added account ' + query)
+        })
+      } else {
+        console.log('found account ', acc)
+        ctx.replyWithMarkdown('account ' + query + ' already being monitored ')
+      }
+    })
+  }
+})
+
+// List all accounts
+app.command('list', ctx => {
+  Account.find((err, accounts) => {
+    if (err) return console.error(err)
+    else {
+      console.log('found accounts: ', accounts)
+      ctx.replyWithMarkdown(JSON.stringify(accounts))
+    }
+  })
+})
 
 module.exports = app
---
+++ models/accountmodel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const accountSchema = mongoose.Schema({
+  account_name: {type: String, required: true, max: 12},
+  last_update: {type: Date, default: Date.now},
+  last_balance: {type: Number}
+})
+
+accountSchema.methods.info = function () {
+  var details = this.account_name
+    ? 'Account: ' + this.account_name
+    : 'No account name found'
+  console.log(details)
+}
+
+module.exports = mongoose.model('SomeModel', accountSchema)
