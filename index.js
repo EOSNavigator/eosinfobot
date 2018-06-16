06b053cc932239c629f549845c624252bf3aec98
Shakhruz Ashirov <shakhruz@gmail.com>
2018-06-16 23:32:43 +0500

    fixes and pretty number of votes number

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ db.once('open', function () {
 const Eos = require('eosjs')
 const config = {
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-  httpEndpoint: 'https://eos.greymass.com',
+  httpEndpoint: 'https://api.eosnewyork.io',
   expireInSeconds: 60,
   broadcast: true,
   debug: false,
